39bb5def5a7258bf68e9be547fdf8aea81a79282
Michieal <michieal@apophissoft.com>
2023-10-11 13:14:08 -0400

    Added License / Copyright notice to code files.

--- Scripts/LogDisplayerNode.cs
+++ Scripts/LogDisplayerNode.cs
@@ -1,15 +1,32 @@
-using Godot;
-using System;
 using System.IO;
-using ApophisSoftware;
+using Godot;
+
+#region License / Copyright
+
+/*
+ * Copyright © 2023, Michieal.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#endregion
 
 namespace ApophisSoftware {
-
 	public partial class LogDisplayerNode : Control {
 		[Export] public Button         btnCloseLogView;
 		[Export] public CodeEdit       LogViewDisplay;
 		[Export] public MenuController MainMenuController;
-
 
 
 		// Called when the node enters the scene tree for the first time.
@@ -24,7 +41,6 @@ namespace ApophisSoftware {
 		}
 
 		private void OnVisibilityChanged() {
-
 			if (!this.Visible) {
 				LogViewDisplay.Text = "";
 				return;
@@ -35,10 +51,10 @@ namespace ApophisSoftware {
 				LogViewDisplay.Text = "Logfile Not Found.";
 				return;
 			}
+
 			StreamReader srLogFile = new StreamReader(LogFile);
 			LogViewDisplay.Text = srLogFile.ReadToEnd();
 			srLogFile.Close();
-
 		}
 	}
 }
--- Scripts/LuaObjects/MetaData.cs
+++ Scripts/LuaObjects/MetaData.cs
@@ -1,6 +1,27 @@
 using System.Collections.Generic;
 using Godot;
 
+#region License / Copyright
+
+/*
+ * Copyright © 2023, Michieal.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#endregion
+
 namespace ApophisSoftware.LuaObjects {
 	public partial class MetaData : RefCounted {
 		internal Dictionary<string, string> metaData = new Dictionary<string, string>();
